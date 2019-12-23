7e1bd8b3b5a5e9986d62fd894622fca2a14f5473
Daniel Santos Silva <56691506+DanielSantos-prog@users.noreply.github.com>
2019-12-23 18:42:26 -0300

    Wifi ESP8266-AcessPoint

--- MR_ESP/MR_ESP.ino
+++ MR_ESP/MR_ESP.ino
@@ -1,121 +1,31 @@
-#include<ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 
+const char* ssid = "Sistema_MR";
+const char* password = "12345678";
 
-const char* ssid = "nomeWifi";
-const char* password = "senha";
-int controle = 0;
-int ligado =0;
-const char* host = "192.168.43.8";
-#define sensorUmidade A0 // Sensor de umidade de solo do módulo
-unsigned long tempoAnterior = 0; // Variável utilizada para guardar o tempo anterior
-unsigned long intervalo = 1000;
-uint8_t GPIO_Pin = D2;
+IPAddress staticIP(192,168,1,22);
+IPAddress gateway(192,168,1,9);
+IPAddress subnet(255,255,255,0);
 
-void ICACHE_RAM_ATTR IntCallback(){
-  digitalWrite(D5,!digitalRead(D5));
-  controle =1;
-  
-  if(digitalRead(D5) == HIGH && ligado==0){ligado=1;}
-  if(digitalRead(D5) == LOW && ligado ==1){ligado=0;}
- 
-}
-
-void setup() {
-  Serial.begin(9600);
-  delay(10);
-  pinMode(D5, OUTPUT);
-
-  attachInterrupt(digitalPinToInterrupt(GPIO_Pin), IntCallback, RISING);
- // Serial.println();
-  //Serial.print("Conectando com");
-  //Serial.println(ssid);
-
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.printf("Connecting to %s\n", ssid);
   WiFi.begin(ssid, password);
-
-  while(WiFi.status() != WL_CONNECTED)
-  {
+  WiFi.config(staticIP, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED)
+    {
     delay(500);
     Serial.print(".");
-   }
-  //Serial.println("");
-  //Serial.print("Wifi Conectado");
-  //Serial.println("Endereço de IP:");
-  //Serial.println(WiFi.localIP());
-  
+    }
+
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-
-    //---------------------
-
-  //----------------------
-  
-  //Serial.print("Conectando com ");
-  //Serial.println(host);
-
-  WiFiClient client;
-
-  const int httpPort = 80;
-  if(!client.connect(host,httpPort))
+void loop() 
   {
-    //Serial.println("Falha na conexao");
-    return;
-    }
-  String url = "/tesla/";
-
-  if(controle ==0){
-         url += "consultlamp.php?";
-         url += "id_lamp=1&id_user=11";
-         url += " ";
-  }else{
-    if(ligado ==1){
-         url += "lamp.php?";
-         url += "status=ON&id=1";
-         url += " ";
-         controle =0;
-         
-    }else{
-         url += "lamp.php?";
-         url += "status=OFF&id=1";
-         url += " ";
-         controle=0;
-      }
-      
-    
-    }  
-         
-   Serial.print("Requisitando URL:");
-   Serial.println(url);
-
-   client.print(String("GET ") + url + "HTTP/1.1\r\n" + "Host:" + host +"\r\n"+ "Connection: close\r\n\r\n");
-
-  unsigned long timeout = millis();
-  while(client.available() ==0)
-  {
-    if(millis() - timeout >5000)
-    {
-      //Serial.println(">>> Client Timeout |");
-      client.stop();
-      return;
-      }
-    
-    }
-   
-      while(client.available())
-      {
-          String line = client.readStringUntil('\r');
-    //      Serial.print(line);
-      //    Serial.flush();
-          if (line.indexOf("OFF") > 0) {
-          digitalWrite(D5,LOW);
-          }
-          if (line.indexOf("ON") > 0) {
-          digitalWrite(D5,HIGH);
-          }
-        }  
-         
-      //Serial.println();
-      //Serial.println("conexao fechada");
-
   
-}
+  }
